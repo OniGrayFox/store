3b362720c3d8f43baa0e056b02721e643b6019d4
onigrayfox <onigrayfox@yandex.ru>
2023-08-21 21:05:26 +0500

    added styles

--- store/urls.py
+++ store/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import index_view
+from products.views import index_view, products_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
-
+    path('products/', products_view, name='products')
 ]
