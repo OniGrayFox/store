e1186a3c74c2fbe6991a7e294a114e162055db65
onigrayfox <onigrayfox@yandex.ru>
2023-08-27 17:11:03 +0500

    added models in admin, rewrite views.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import site
+from .models import ProductCategory, Product
 
-# Register your models here.
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'quantity', 'image', 'category']
+
+
+site.register(ProductCategory, ProductCategoryAdmin)
+site.register(Product, ProductAdmin)
