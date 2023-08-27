24bbd91077f05c2b13d35917b5c0a46ccb794d87
onigrayfox <onigrayfox@yandex.ru>
2023-08-27 19:24:12 +0500

    changed settings.py for images urls

--- store/urls.py
+++ store/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 from products.views import index_view, products_view
-
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
     path('products/', products_view, name='products')
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
