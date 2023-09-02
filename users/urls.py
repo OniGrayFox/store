d41903b26b6c1dee66b05c8d85eb837d3c3ba127
onigrayfox <onigrayfox@yandex.ru>
2023-09-02 19:40:13 +0500

    added forms.py, added opportunity to log-in

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = 'users'
 
 urlpatterns = [
 
-    path('/login', login_view, name='login'),
-    path('/register', registration_view, name='register'),
+    path('login', login_view, name='login'),
+    path('register', registration_view, name='register'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from users.models import User
+from users.forms import UserLoginForm
+from django.urls import reverse
 
 
 def login_view(request):
-    return render(request, "users/login.html")
+
+    if request.POST:
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    args = {
+        'form': form,
+
+            }
+    return render(request, "users/login.html", args)
 
 
 def registration_view(request):
