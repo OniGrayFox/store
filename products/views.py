18344ed73115c53c8759a6cc4a893e42c96646a9
onigrayfox <onigrayfox@yandex.ru>
2023-10-05 21:23:44 +0500

    rewrite Catalog view from FBV to CBV, changed following files

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.http.response import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.views.generic import TemplateView
-
+from django.views.generic.list import ListView
 
 class IndexView(TemplateView):
     template_name = 'products/index.html'
@@ -24,23 +24,41 @@ class IndexView(TemplateView):
 #     }
 #     return render(request, "products/index.html", args)
 
+class ProductsListView(ListView):
+    model = Product
+    template_name = "products/products.html"
+    paginate_by = 2
 
-def products_view(request, category_id=None, page_number=1):
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)  # К id можно через _ слэш а не через __
-    else:
-        products = Product.objects.all()
-    per_page = 2
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
 
 
-    args = {
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all()
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data()
+        context['title'] = 'Store - каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
-    }
-    return render(request, "products/products.html", args)
+
+
+# def products_view(request, category_id=None, page_number=1):
+#     if category_id:
+#         products = Product.objects.filter(category_id=category_id)  # К id можно через _ слэш а не через __
+#     else:
+#         products = Product.objects.all()
+#     per_page = 2
+#     paginator = Paginator(products, per_page)
+#     products_paginator = paginator.page(page_number)
+#
+#
+#     args = {
+#         'products': products_paginator,
+#         'categories': ProductCategory.objects.all()
+#
+#     }
+#     return render(request, "products/products.html", args)
 
 
 @login_required
