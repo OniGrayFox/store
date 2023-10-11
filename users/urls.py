37194ce117530bf090b9abd5052484ad3b7b4714
onigrayfox <onigrayfox@yandex.ru>
2023-10-11 19:25:07 +0500

    Rewrite to CBV profile and login

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
 from django.urls import path
-from users.views import login_view, UserRegistrationView, profile_view, logout_view
+from users.views import login_view, UserRegistrationView, UserProfileView, logout_view
 
+from django.contrib.auth.decorators import login_required
 app_name = 'users'
 
 urlpatterns = [
 
-    path('login', login_view, name='login'),
-    path('register', UserRegistrationView.as_view(), name='register'),
-    path('profile', profile_view, name='profile'),
-    path('logout', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
+    path('logout/', logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,9 +4,7 @@ from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.urls import reverse, reverse_lazy
 from products.models import Basket
-from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, UpdateView
 
 
 def login_view(request):
@@ -38,6 +36,18 @@ class UserRegistrationView(CreateView):
     success_url = reverse_lazy('users:login')
 
 
+class UserProfileView(UpdateView):
+    model = User
+    template_name = 'users/profile.html'
+    form_class = UserProfileForm
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['baskets'] = Basket.objects.filter(user=self.request.user)
+        return context
 
 # def registration_view(request):
 #     if request.POST:
@@ -50,28 +60,26 @@ class UserRegistrationView(CreateView):
 #         form = UserRegistrationForm()
 #
 #     args = {
-#
 #         "form": form,
-#
 #     }
 #     return render(request, "users/register.html", args)
 
-@login_required
-def profile_view(request):
-    if request.POST:
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse("users:profile"))
-    else:
-        form = UserProfileForm(instance=request.user)
-
-    args = {
-        'form': form,
-        'baskets': Basket.objects.filter(user=request.user),
-
-            }
-    return render(request, "users/profile.html", args)
+# @login_required
+# def profile_view(request):
+#     if request.POST:
+#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse("users:profile"))
+#     else:
+#         form = UserProfileForm(instance=request.user)
+#
+#     args = {
+#         'form': form,
+#         'baskets': Basket.objects.filter(user=request.user),
+#
+#             }
+#     return render(request, "users/profile.html", args)
 
 
 def logout_view(request):
