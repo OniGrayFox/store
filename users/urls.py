dee79dbcdb59751c4dfab62673708f7e11204587
onigrayfox <onigrayfox@yandex.ru>
2023-10-18 21:58:10 +0500

    Changed login and logout to CBV

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 
 from django.urls import path
-from users.views import login_view, UserRegistrationView, UserProfileView, logout_view
+from users.views import UserLoginView, UserRegistrationView, UserProfileView
+
+from django.contrib.auth.views import LogoutView
 
 from django.contrib.auth.decorators import login_required
 app_name = 'users'
 
 urlpatterns = [
 
-    path('login/', login_view, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
-    path('logout/', logout_view, name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -5,27 +5,12 @@ from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.urls import reverse, reverse_lazy
 from products.models import Basket
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
 
-def login_view(request):
-
-    if request.POST:
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-
-    args = {
-        'form': form,
-
-            }
-    return render(request, "users/login.html", args)
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+    form_class = UserLoginForm
 
 
 
@@ -48,42 +33,4 @@ class UserProfileView(UpdateView):
         context = super(UserProfileView, self).get_context_data()
         context['baskets'] = Basket.objects.filter(user=self.request.user)
         return context
-
-# def registration_view(request):
-#     if request.POST:
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, "Поздравляем! пользователь создан!")
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegistrationForm()
-#
-#     args = {
-#         "form": form,
-#     }
-#     return render(request, "users/register.html", args)
-
-# @login_required
-# def profile_view(request):
-#     if request.POST:
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse("users:profile"))
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#
-#     args = {
-#         'form': form,
-#         'baskets': Basket.objects.filter(user=request.user),
-#
-#             }
-#     return render(request, "users/profile.html", args)
-
-
-def logout_view(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
-
 
