d43fd3ad35d11506eadd57b4076c6780b1f34249
onigrayfox <onigrayfox@yandex.ru>
2023-12-07 21:47:57 +0500

    created tests for UserRegistration

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
+from http import HTTPStatus
 from django.test import TestCase
+from django.urls import reverse
+from users.models import EmailVerification, User
 
-# Create your tests here.
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self):
+        self.path = reverse('user:registration')
+
+    def test_user_reg_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_user_registration_post_success(self):
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        # check creating of user
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
